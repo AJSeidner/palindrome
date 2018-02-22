9f95936cfbc19086abe95a2bdb414ea85ab7b722
AJSeidner <AJSeidner@gmail.com>
2018-02-22 07:38:43 -0600

    javascript/html updates

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ window.onload = function() {
 var word1 = [];
 var word2 = [];
 
-var word = document.getElementById("userWord");
-function palindromeCheck() {
-	word1 = word.split("");
-	word2 = word1.reverse();
-	var word3 = word1.join();
-	var word4 =word2.join();
 
-	if (word3 === word4){
+document.getElementById("button").onclick = function() {palindromeCheck()};
+
+
+function palindromeCheck() {
+	var word = document.getElementById("userWord").value;
+	var word1 = word.split("");
+	var word2 = word1.reverse();
+	var word3 = word2.join("");
+
+	if (word3 === word){
 		console.log("it's a palindrome");
 	}else {
 		console.log("no palindrome here");
 	};
 };
-document.getElementById("button").onclick = function() {palindromeCheck()};
 
-
+console.log("test prefunction");
 };
+console.log("test postfunction");
